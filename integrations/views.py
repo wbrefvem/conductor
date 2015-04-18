da2b00e66bc6daa4452f7cb28797752528e43ebc
Will Refvem <wbrefvem@gmail.com>
2015-04-18 19:42:35 -0400

    Minor updates to views

--- integrations/views.py
+++ integrations/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseNotAllowed
 from integrations.tasks import notify_at_task
 import json
 import pprint
+import requests
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -10,9 +11,26 @@ pp = pprint.PrettyPrinter(indent=4)
 class AtTaskNotificationView(View):
     template_name = 'template.html'
 
+    def login(username='', password=''):
+        resp = requests.get('https://raleighnc-it.attask-ondemand.com/attask/api/login?username=%s&password=%s' % (username, password))
+        data = resp.json()['data']
+        return data['sessionID']
+
+    def logout(session_id=''):
+        resp = requests.get('https://raleighnc-it.attask-ondemand.com/attask/api/logout?sessionID=%s' % session_id)
+        success = resp.json()['data']['success']
+
+        if not success:
+            raise NetworkError('Logout failed!')
+
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
-        pp.pprint(data)
+
+        note_string = '[GitHub] %s' % data['comment']['body']
+
+        session_id = self.login('will.refvem@raleighnc.gov', 'Welcome2')
+
+        resp = requests.post('https://raleighnc-it.attask-ondemand.com/attask/api/')
 
         return HttpResponse('AtTask notification initiated...')
 
