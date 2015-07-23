2a54adc8de49e80260de21d42ca02e106b30822e
Will Refvem <wbrefvem@gmail.com>
2015-07-23 15:20:32 -0400

    Updates to urls

--- conductor/urls.py
+++ conductor/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 
 
 urlpatterns = [
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('integrations.urls'))
+    url(r'^', include('integrations.urls')),
+    # url(r'^', include('api.urls')),
+    url(r'^', include('django.contrib.auth.urls'))
 ]
